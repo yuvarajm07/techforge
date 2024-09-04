ae24af5ecb9581442ba0de65d887085239d9c247
yuvarajm07 <144088641+yuvarajm07@users.noreply.github.com>
2024-09-04 12:32:27 +0530

    Add files via upload

---
+++ test12.py
@@ -0,0 +1,18 @@
+user_pass=[{"user name":"yuva","password":"yuva@123"},{"user name":"ajay","password":"ajay@123"},{"user name":"vishnu","password":"vishnu@123"}]
+
+while True:
+    ent=input("Enter your user name :")
+    pas=input("enter the password:")
+    if ent == user_pass[0]["user name"] and pas==user_pass[0]["password"] :
+            print("welcome!!")      
+            
+    elif ent == user_pass[1]["user name"] and pas==user_pass[1]["password"] :
+            print("welcome!!")
+
+    elif ent == user_pass[2]["user name"] and pas==user_pass[2]["password"] :
+            print("welcome!!")
+    
+    else:
+        print("invalid enteries")
+        
+    break
